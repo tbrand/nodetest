d56a0b30c371aae36eb802ff851cab4cf46cfb61
tbrand <taichiro0709@gmail.com>
2018-02-02 00:09:42 +0900

    fix

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const cluster = require('cluster');
 const mining = function(url, address, nprocesses) {
     if (cluster.isMaster) {
 	const client = require('./lib/client');
-
-	client.setup();
 	client.connect(url, address, nprocesses);
     } else {
 	require('./lib/miner');
