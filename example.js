f8e1ddc7a25600ac6cffb6a40d51069e30ca0026
tbrand <taichiro0709@gmail.com>
2018-02-01 22:52:11 +0900

    fix

---
+++ example.js
@@ -0,0 +1,3 @@
+const mining = require("./index");
+
+mining('http://127.0.0.1:3000', 2);
--- index.js
+++ index.js
@@ -1,24 +1,14 @@
 const cluster = require('cluster');
 
-if (cluster.isMaster) {
-  const client = require('./lib/client');
-  const url = 'http://127.0.0.1:3000';
-  const nprocesses = 2;
+const mining = function(url, nprocesses) {
+    if (cluster.isMaster) {
+	const client = require('./lib/client');
 
-  for (let i = 0 ; i < nprocesses ; i ++) {
-	  const process = cluster.fork();
-  }
-
-  const messageHandler = function(message) {
-	  console.log(`nonce coming!: ${message.nonce}`);
-  };
-
-  cluster.on('exit', function(worker, code, signal) {
-    console.log('worker ' + worker.process.pid + ' died');
-  });
-
-  client.setup();
-  client.connect(url);
-} else {
-  require('./lib/miner');
+	client.setup();
+	client.connect(url, nprocesses);
+    } else {
+	require('./lib/miner');
+    }
 }
+
+module.exports = mining;
