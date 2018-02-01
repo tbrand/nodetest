8d18f5b650e49fa3868b405022a51e8a57ce3437
taicsuzu <taicsuzu@yahoo-corp.jp>
2018-02-01 17:37:23 +0900

    fix

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const cluster = require('cluster');
 
 if (cluster.isMaster) {
-    const client = require('./lib/client');
-    const url = 'http://127.0.0.1:3000';
-    const nprocesses = 2;
+  const client = require('./lib/client');
+  const url = 'http://127.0.0.1:3000';
+  const nprocesses = 2;
 
-    for (let i = 0 ; i < nprocesses ; i ++) {
-	const process = cluster.fork();
-    }
+  for (let i = 0 ; i < nprocesses ; i ++) {
+	  const process = cluster.fork();
+  }
 
-    const messageHandler = function(message) {
-	console.log(`nonce coming!: ${message.nonce}`);
-    };
+  const messageHandler = function(message) {
+	  console.log(`nonce coming!: ${message.nonce}`);
+  };
 
-    cluster.on('exit', function(worker, code, signal) {
-        console.log('worker ' + worker.process.pid + ' died');
-    });
+  cluster.on('exit', function(worker, code, signal) {
+    console.log('worker ' + worker.process.pid + ' died');
+  });
 
-    client.setup();
-    client.connect(url);
+  client.setup();
+  client.connect(url);
 } else {
-    require('./lib/miner');
+  require('./lib/miner');
 }
